e508617ad884846baa140430c5f578dd2873349f
Mardiprk <mk1659770@gmail.com>
2021-08-30 18:30:57 +0530

    Fix try 4

--- pages/components/layout.js
+++ pages/components/layout.js
@@ -1,10 +1,8 @@
-import NavBar from "./NavBar";
 import Footer from "./Footer";
 
 export default function Layout({ children }) {
   return (
     <div className="content">
-      <NavBar />
       {children}
       <Footer />
     </div>
