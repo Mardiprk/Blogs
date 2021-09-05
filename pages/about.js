9bedae448fa051322b31c39b7ca203743d0f7253
Mardiprk <mk1659770@gmail.com>
2021-09-05 15:10:47 +0530

    Cont

--- pages/about.js
+++ pages/about.js
@@ -5,15 +5,15 @@ export default function about() {
       <div className="w3-row-padding" style={{ padding: 10 }}>
         <div className="w3-third">
           <p style={{ padding: 10 }}>
-            {The Paper News App provides access to the Live News Updates, with
+            The Paper News App provides access to the Live News Updates, with
             the latest news for events happening across the globe and in India.
             Keep Trends in your pocket. Get summarized content across various
-            categories and interests. There's no in-app purchases, no need to
+            categories and interests. There no in-app purchases, no need to
             login/sign-up, no notifications, no intrusion. Its Simply News! With
             the goal of making news available to most in their own languages.
             Simple interface to save your time with byte sized information shown
             in single page from trusted sources from across the globe. Paper
-            News is fully developed and managed by an indie app developer.}
+            News is fully developed and managed by an indie app developer.
           </p>
         </div>
       </div>
